ae1373816566a51ac8d309f8943c0e872864be08
Artem Tishenko <artem.200207@yandex.ru>
2023-03-06 22:46:09 +0400

    first commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface CardProps {
+    width: string;
+    height: string;
+    children: React.ReactNode;
+}
+
+const Card: React.FC<CardProps> = ({width, height, children}) => {
+  return (
+    <div style={{width, height, border: '1px solid gray'}}>{children}</div>
+  )
+}
+
+export default Card
