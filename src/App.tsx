0323dadeced32405cf90ebb4eca5345f1f89a6d0
Artem Tishenko <artem.200207@yandex.ru>
2023-03-06 22:58:34 +0400

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import Card from "./components/Card";
+import Card, { CardVariant } from "./components/Card";
 
 
 function App() {
   return (
     <div className="App">
-      <Card width="200px" height="200px">
+      <Card width="200px" height="200px" variant={CardVariant.outlinde} clickFunc={(num) => console.log('click ' + num)
+      }>
         <button>Btn</button>
       </Card>
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+export enum CardVariant {
+    outlinde = 'outlined',
+    primary = 'primary'
+}
 
 interface CardProps {
     width: string;
     height: string;
-    children: React.ReactNode;
+    variant: CardVariant;
+    children?: React.ReactNode;
+    clickFunc: (num: number) => void;
 }
 
-const Card: React.FC<CardProps> = ({width, height, children}) => {
-  return (
-    <div style={{width, height, border: '1px solid gray'}}>{children}</div>
-  )
+const Card: React.FC<CardProps> = ({ width, height, children, variant, clickFunc }) => {
+
+    const [state, setState] = useState(0)
+    return (
+        <div onClick={() => clickFunc(state)}
+         style={{
+            width, height, border: variant === CardVariant.outlinde ? '1px solid gray' : 'none',
+        background: variant === CardVariant.primary ? 'lightgray' : ''
+        }}>{children}</div>
+    )
 }
 
 export default Card
